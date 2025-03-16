fe7b03002c41af4c455f6e4d45c73cab3547baa8
Andre Filipe <andre.filipe_01@hotmail.com>
2025-03-16 19:33:46 +0000

    better start and setup

--- my-app/start.sh
+++ my-app/start.sh
@@ -3,47 +3,18 @@
 # Set working directory to the script's location
 cd "$(dirname "$0")"
 
-echo "Checking if MongoDB container exists..."
-if [ "$(docker ps -a | grep mongo-container)" ]; then
-  echo "MongoDB container already exists"
-  
-  # Check if container is running
-  if [ "$(docker ps | grep mongo-container)" ]; then
-    echo "MongoDB container is already running"
-  else
-    echo "Starting existing MongoDB container..."
-    docker start mongo-container
-  fi
-else
-  echo "Creating and starting new MongoDB container..."
-  docker run -d --name mongo-container -p 27017:27017 \
-    -e MONGO_INITDB_ROOT_USERNAME=admin \
-    -e MONGO_INITDB_ROOT_PASSWORD=secret \
-    mongo:latest
-fi
-
-# Set up client
-echo "Setting up client application..."
-cd client
-echo "Installing client dependencies..."
-npm install
-
 # Start client in background
 echo "Starting client application..."
+cd client
 npm run dev &
 CLIENT_PID=$!
 
 # Return to base directory
 cd ..
 
-# Set up server
-echo "Setting up server application..."
-cd server
-echo "Installing server dependencies..."
-npm install
-
 # Start server
 echo "Starting server application..."
+cd server
 node server.js
 
 # This section will execute if server.js exits
